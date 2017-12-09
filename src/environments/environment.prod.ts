a3aae30271b4d2fd0231bb14288414bc97f7a0fe
Thomas <thomas.peyrard@gmail.com>
2017-12-09 22:17:36 +0100

    linting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB51Ob3dng2p3KDRppzKV-628mwLrsBNz4",
-    authDomain: "baby-amenities.firebaseapp.com",
-    databaseURL: "https://baby-amenities.firebaseio.com",
-    projectId: "baby-amenities",
-    storageBucket: "",
-    messagingSenderId: "673805698219"
+    apiKey: 'AIzaSyB51Ob3dng2p3KDRppzKV-628mwLrsBNz4',
+    authDomain: 'baby-amenities.firebaseapp.com',
+    databaseURL: 'https://baby-amenities.firebaseio.com',
+    projectId: 'baby-amenities',
+    storageBucket: '',
+    messagingSenderId: '673805698219'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB51Ob3dng2p3KDRppzKV-628mwLrsBNz4",
-    authDomain: "baby-amenities.firebaseapp.com",
-    databaseURL: "https://baby-amenities.firebaseio.com",
-    projectId: "baby-amenities",
-    storageBucket: "",
-    messagingSenderId: "673805698219"
+    apiKey: 'AIzaSyB51Ob3dng2p3KDRppzKV-628mwLrsBNz4',
+    authDomain: 'baby-amenities.firebaseapp.com',
+    databaseURL: 'https://baby-amenities.firebaseio.com',
+    projectId: 'baby-amenities',
+    storageBucket: '',
+    messagingSenderId: '673805698219'
   }
 };
