135d2a5f766ddc2fd01ea6c9d28120fdf52e87d7
Thomas <thomas.peyrard@gmail.com>
2017-12-11 22:38:31 +0100

    Fix wrong configuration variable for Karma tests in order to run on Travis

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
   });
 
   if(process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
+    config.browsers = ['Chrome_travis_ci'];
   }
-  
+
 };
