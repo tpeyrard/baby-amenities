5264c4dd0a80e59f02ee8b27bcb880536ef95b0f
Thomas <thomas.peyrard@gmail.com>
2017-12-19 23:27:50 +0100

    Sign-in with Google to Firebase

--- src/app/amenities.service.ts
+++ src/app/amenities.service.ts
@@ -1,14 +1,29 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireAuth} from "angularfire2/auth";
+import * as firebase from "firebase";
+import {User} from "firebase";
 
 @Injectable()
 export class AmenitiesService {
 
-  constructor(private database: AngularFireDatabase) {
+  constructor(private database: AngularFireDatabase, private afAuth: AngularFireAuth) {
   }
 
   getArticles(): Observable<any[]> {
     return this.database.list('/baby-amenities').valueChanges();
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  authenticationState(): Observable<User>{
+    return this.afAuth.authState;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,41 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AmenitiesService} from "./amenities.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Amenities';
   signedIn = false;
-  computeLock() : string {
+  user = null;
+
+  constructor(private amenitiesService: AmenitiesService) {
+  }
+
+  ngOnInit() {
+    this.amenitiesService
+      .authenticationState()
+      .subscribe(user => {
+        this.user = user;
+        this.signedIn = (user != null)
+      });
+  }
+
+  computeLock(): string {
     return this.signedIn ? 'lock' : 'lock_open';
   }
 
-  login(): void {
-    this.signedIn = true;
-    console.log("Login: " + this.signedIn);
+  signIn(): void {
+    if (!this.signedIn) {
+      this.amenitiesService.login();
+    }
+  }
+
+  signOut(): void {
+    if (this.signedIn) {
+      this.amenitiesService.logout();
+    }
   }
 }
