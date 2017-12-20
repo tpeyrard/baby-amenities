6fe01e939bed520d7af29bc59b2cc40b732c78fd
Thomas PEYRARD <thomas.peyrard@gmail.com>
2017-12-20 15:02:16 +0100

    Fix: wrong path for firebase items

--- src/app/amenities.service.ts
+++ src/app/amenities.service.ts
@@ -12,7 +12,7 @@ export class AmenitiesService {
   }
 
   getArticles(): Observable<any[]> {
-    return this.database.list('/baby-amenities').valueChanges();
+    return this.database.list('/').valueChanges();
   }
 
   login() {
