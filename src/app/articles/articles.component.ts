972e9bd93c396bebce996b285790e51f1c6aa369
Thomas PEYRARD <thomas.peyrard@gmail.com>
2018-01-03 17:13:36 +0100

    Feat: add default images based on category

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,8 +1,21 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AmenitiesService} from "../amenities.service";
 import {Observable} from "rxjs/Observable";
-import {MediaMatcher} from "@angular/cdk/layout";
 import {Article} from "../article";
+
+const CAT_TO_IMAGE = {
+  'habits': '/assets/images/jean.jpg',
+  'body': '/assets/images/body.jpg',
+  'jouet': '/assets/images/velo.jpg',
+  'poussette': '/assets/images/poussette.jpg',
+  'rangement': '/assets/images/coffre.jpg',
+  'maman': '/assets/images/enceinte.jpg',
+  'dehors': '/assets/images/bonnet.jpg',
+  'naissance': '/assets/images/berceau.jpg',
+  'nuit': '/assets/images/lit.jpg',
+  'repas': '/assets/images/repas.jpg',
+  'chambre': '/assets/images/chambre.jpg'
+};
 
 @Component({
   selector: 'app-articles',
@@ -13,7 +26,8 @@ export class ArticlesComponent implements OnInit {
 
   public articles: Observable<Article[]>;
 
-  constructor(private amenitiesService: AmenitiesService) {  }
+  constructor(private amenitiesService: AmenitiesService) {
+  }
 
   ngOnInit() {
     this.articles = this.amenitiesService.getArticles();
@@ -25,5 +39,9 @@ export class ArticlesComponent implements OnInit {
 
   removeArticle(id: string) {
     this.amenitiesService.remove(id);
+  }
+
+  getImage(article: Article): string {
+    return article.image || CAT_TO_IMAGE[article.category] || CAT_TO_IMAGE['jouet']
   }
 }
