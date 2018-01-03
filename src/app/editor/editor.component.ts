bbc41f3e379680766fc68fc67c939b3198445ad8
Thomas PEYRARD <thomas.peyrard@gmail.com>
2018-01-03 13:50:19 +0100

    Display: increase editor width

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -20,7 +20,7 @@ export class EditorComponent implements OnInit {
 
   open(): void {
     let dialogRef = this.dialog.open(DialogOverview, {
-      width: '250px',
+      width: '400px',
       data: new Article()
     });
 
@@ -36,23 +36,23 @@ export class EditorComponent implements OnInit {
   template: `
     <h1 mat-dialog-title>Add a new article</h1>
     <div mat-dialog-content>
-      <mat-form-field>
+      <mat-form-field style="width: 100%">
         <mat-label>Name</mat-label>
         <input matInput tabindex="1" [(ngModel)]="article.name" placeholder="Name" required>
       </mat-form-field>
-      <mat-form-field>
+      <mat-form-field style="width: 100%">
         <mat-label>Size</mat-label>
         <input matInput tabindex="2" [(ngModel)]="article.size" placeholder="Size">
       </mat-form-field>
-      <mat-form-field>
-        <mat-label>Value</mat-label>
+      <mat-form-field style="width: 100%">
+        <mat-label>Value / Price</mat-label>
         <input matInput tabindex="3" [(ngModel)]="article.value" placeholder="Value">
       </mat-form-field>
-      <mat-form-field>
-        <mat-label>Image</mat-label>
+      <mat-form-field style="width: 100%">
+        <mat-label>Image URL</mat-label>
         <input matInput tabindex="4" [(ngModel)]="article.image" placeholder="Image">
       </mat-form-field>
-      <mat-form-field>
+      <mat-form-field style="width: 100%">
         <mat-label>Description</mat-label>
         <textarea matInput tabindex="5" [(ngModel)]="article.desc"
                   placeholder="Description" matTextareaAutosize matAutosizeMinRows="2" matAutosizeMaxRows="5">
@@ -60,9 +60,9 @@ export class EditorComponent implements OnInit {
         </textarea>
       </mat-form-field>
     </div>
-    <div mat-dialog-actions>
-      <button mat-button [mat-dialog-close]="article" tabindex="6">Create</button>
-      <button mat-button (click)="onNoClick()" tabindex="-1">Cancel</button>
+    <div mat-dialog-actions fxLayoutAlign="center">
+      <button mat-button color="primary" [mat-dialog-close]="article" tabindex="6">Create</button>
+      <button mat-button color="warn" (click)="onNoClick()" tabindex="-1">Cancel</button>
     </div>
   `,
 })
