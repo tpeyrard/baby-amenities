41100bb597f0b4fe3f5ac5375d6d6ee68ca4a32c
Thomas <thomas.peyrard@gmail.com>
2018-01-14 14:43:15 +0100

    Update account component to use the same display as the articles one. TODO: factorize the template between both components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,33 +1,34 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AmenitiesService} from "../amenities.service";
 import {Observable} from "rxjs/Observable";
-import {Article} from "../article";
-import {MediaMatcher} from "@angular/cdk/layout";
+import {Article, CAT_TO_IMAGE} from "../article";
 
 @Component({
   selector: 'app-account',
   template: `
-      <div class="nav-content">
-        <h2 *ngIf="(userArticles | async)?.length == 0" style="text-align: center">Aucun article sélectionné.</h2>
-        <ng-container *ngFor="let article of userArticles | async">
-          <mat-card class="card" tabindex="0">
-            <mat-card-header>
-              <mat-icon mat-card-avatar>{{article.category}}</mat-icon>
-              <mat-card-title>{{article.name}}</mat-card-title>
-              <mat-card-subtitle>{{article.size}} - {{article.value}}</mat-card-subtitle>
-            </mat-card-header>
-            <mat-card-content class="card-content">
-              <img class="card-image" src="{{article.image}}" alt="Product image">
-              <p [innerText]="article.desc" class="card-desc"></p>
-            </mat-card-content>
-            <mat-card-actions class="card-actions" align="end">
-              <button mat-button color="primary" (click)="confirmPurchase(article)">Acheté</button>
-              <button mat-button color="warn" (click)='cancel(article)'>Annuler</button>
-            </mat-card-actions>
-          </mat-card>
+    <div class="nav-content">
+      <h2 *ngIf="(userArticles | async)?.length == 0" style="text-align: center">Aucun article sélectionné.</h2>
+      <ng-container *ngFor="let article of userArticles | async">
+        <mat-card class="card" tabindex="0">
+          <mat-card-header>
+            <mat-icon mat-card-avatar>{{article.category}}</mat-icon>
+            <mat-card-title>{{article.name}}</mat-card-title>
+            <mat-card-subtitle>{{article.size}} - {{article.value}}</mat-card-subtitle>
+          </mat-card-header>
+          <div class="img-container">
+            <img mat-card-image src="{{getImage(article)}}" alt="Photo of the product">
+          </div>
+          <mat-card-content class="card-content">
+            <p [innerText]="article.desc" class="card-desc"></p>
+          </mat-card-content>
+          <mat-card-actions class="card-actions" align="end">
+            <button mat-button color="primary" (click)="confirmPurchase(article)">Acheté</button>
+            <button mat-button color="warn" (click)='cancel(article)'>Annuler</button>
+          </mat-card-actions>
+        </mat-card>
 
-        </ng-container>
-      </div>
+      </ng-container>
+    </div>
   `,
   styles: [
       ``]
@@ -35,7 +36,8 @@ import {MediaMatcher} from "@angular/cdk/layout";
 export class AccountComponent implements OnInit {
   public userArticles: Observable<Article[]>;
 
-  constructor(private amenitiesService: AmenitiesService) {  }
+  constructor(private amenitiesService: AmenitiesService) {
+  }
 
   ngOnInit() {
     this.userArticles = this.amenitiesService.getArticlesForCurrentUser();
@@ -47,5 +49,9 @@ export class AccountComponent implements OnInit {
 
   cancel(article: Article) {
     this.amenitiesService.release(article);
+  }
+
+  getImage(article: Article): string {
+    return article.image || CAT_TO_IMAGE[article.category] || CAT_TO_IMAGE['jouet']
   }
 }
